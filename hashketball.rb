267e1a0fd59c5c5ed7e30a19769da89dcae23ad8
Mark Pothecary <markpothers@hotmail.com>
2019-02-14 20:14:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,7 +182,6 @@ def big_shoe_rebounds
     game_hash.each do |team, team_param|
       team_param[:players].each do |name, stats|
         shoesizes << stats[:shoe]
-        binding.pry
       end
 puts shoesizes
     end
